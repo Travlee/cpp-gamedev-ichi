3fc51aefc8d5a94851f5ed9185c616ce4131bae9
Travis Lee Presnell <travleepresnell@gmail.com>
2026-04-02 12:36:18 -0400

    feat: added imgui and rlimgui libs
    Added: some test code for ImGui and rlimgui requirements.
    Added: Custom CMakeLists for both ImGui and rlImGui.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <raylib.h>
+#include "raylib.h"
+#include "imgui.h"
+#include "rlimgui.h"
 
 int main()
 {
@@ -7,16 +9,33 @@ int main()
 
         InitWindow(800, 450, "Ichi");
 
+        rlImGuiSetup(true);
+
         while(!WindowShouldClose()){
                 BeginDrawing();
 
                 ClearBackground(RAYWHITE);
 
+                rlImGuiBegin();
+
+                // ! Test
+                DrawRectangle(75, 75, 100, 100, {0, 255, 0, 127});
+                DrawRectangle(50, 50, 100, 100, {255, 0, 0, 127});
+
                 DrawText("Ichi text", 190, 200, 20, RED);
 
+                // ? IMGUI Test
+                ImGui::Begin("test");
+                ImGui::Text("hello");
+                ImGui::Button("button");
+                ImGui::ShowDemoWindow();
+                ImGui::End();
+
+                rlImGuiEnd();
                 EndDrawing();
         }
 
+        rlImGuiShutdown();
         CloseWindow();
 
         return 0;
